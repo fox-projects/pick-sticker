154ebf32525d0d4b1219f79c2b91ad9d9c138c09
Edwin Kofler <edwin@kofler.dev>
2022-01-13 02:11:15 -0800

    feat: Add simple resizer script

---
+++ resizer/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from pathlib import Path
+from PIL import Image
+from colorama import init, Fore, Back, Style
+
+init()
+
+def info(msg: str) -> None:
+	print(f"{Fore.BLUE}Info{Fore.RESET}: {msg}")
+
+# -------------------------------------------------------- #
+#                           START                          #
+# -------------------------------------------------------- #
+DOWNLOADS_DIR = '../downloads'
+size = 150
+
+for webp_image in Path(DOWNLOADS_DIR).rglob("*.webp"):
+	png_image = str(webp_image.parent / webp_image.stem) + ".png"
+	png_image_small = str(webp_image.parent / webp_image.stem) + "_small.png"
+
+	info(f"Processing {webp_image}")
+	with Image.open(webp_image) as im:
+		if not Path(png_image).exists():
+			im.save(png_image)
+
+		# if not Path(png_image_small).exists():
+		im.resize((size, size)).save(png_image_small)
